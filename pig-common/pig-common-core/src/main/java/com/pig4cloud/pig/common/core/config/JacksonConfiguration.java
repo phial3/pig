b5ec3f4aae98fc64f13c056c10085a7ac79b9a0d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-04 10:35:29 +0800

    :sparkles: 将储存在 Redis 中的数据序列化为 JSON（本功能不兼容之前的Redis数据，需要将原有Redis数据清空才能正常使用） https://github.com/spring-projects/spring-security/issues/4370

--- pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/config/JacksonConfiguration.java
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/config/JacksonConfiguration.java
@@ -17,15 +17,10 @@
 package com.pig4cloud.pig.common.core.config;
 
 import cn.hutool.core.date.DatePattern;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.pig4cloud.pig.common.core.jackson.PigJavaTimeModule;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.time.ZoneId;
@@ -40,9 +35,9 @@ import java.util.TimeZone;
  * @author lishangbu
  * @date 2020-06-17
  */
-@AutoConfiguration
-@ConditionalOnClass(ObjectMapper.class)
-@AutoConfigureBefore(JacksonAutoConfiguration.class)
+// @EnableCaching
+// @AutoConfiguration
+// @AutoConfigureBefore(RedisAutoConfiguration.class)
 public class JacksonConfiguration {
 
 	@Bean
